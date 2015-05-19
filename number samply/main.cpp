41bbb36931cb8002ea99d1bd7bc487ef228af052
rilftech0 <bduffytech@gmail.com>
2015-05-19 14:48:37 -0800

    Inshly commit

---
+++ number samply/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	cout<<"enter any number you want\n";
+	int a=1;
+	
+	cin>> a;
+	
+	while(a>0)
+	{
+		cout<< "enter any number you want.\n";
+		
+		cin>> a;
+	}
+	cout<< "Nice job.\n";
+
+	char cc;
+	cin >> cc;
+}
